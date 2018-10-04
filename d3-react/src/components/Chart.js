9b436e2a763785187061f3a243f14a151cb958c3
Marius Stokseth <mariussto@netcompany.com>
2018-10-04 18:01:28 +0200

    la til backend

--- d3-react/src/components/Chart.js
+++ d3-react/src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../css/Chart.css'
 import Norway from 'norway.js';
 import d3 from 'd3';
@@ -8,16 +8,16 @@ export default class Chart extends Component {
   componentDidMount() {
     const el = document.getElementById("norge");
 
-    const debounce = function(fn, wait = 10) {
+    const debounce = function (fn, wait = 10) {
       let timeout;
-      return function() {
+      return function () {
         var args = arguments;
         clearTimeout(timeout);
         timeout = setTimeout(() => fn.apply(this, args), wait);
       };
     };
 
-    const norway = new Norway(el, { area : Norway.Municipalities });
+    const norway = new Norway(el, {area: Norway.Municipalities});
 
     const size = () => el.style.height = document.body.parentNode.clientHeight + "px";
     size();
@@ -39,22 +39,22 @@ export default class Chart extends Component {
       var letters = '0123456789ABCDEF';
       var color = '#';
       for (var i = 0; i < 6; i++) {
-        color += letters[ Math.floor(Math.random() * 16) ];
+        color += letters[Math.floor(Math.random() * 16)];
       }
       return color;
     }
 
-    }
+  }
 
   render() {
     return (
       <div>
         <p id={"valgt"}></p>
-        <div id={"norge"} />
+        <div id={"norge"}/>
         <svg className={"d3-svg"} width="300px" height="150px">
-          <rect x="70" y="20" width="20px" height="20" rx="5" ry="5" />
-          <rect x="140" y="20" width="20px" height="20" rx="5" ry="5" />
-          <rect x="210" y="20" width="20px" height="20" rx="5" ry="5" />
+          <rect x="70" y="20" width="20px" height="20" rx="5" ry="5"/>
+          <rect x="140" y="20" width="20px" height="20" rx="5" ry="5"/>
+          <rect x="210" y="20" width="20px" height="20" rx="5" ry="5"/>
         </svg>
 
       </div>
